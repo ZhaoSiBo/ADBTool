00637856baffdc9993d219ad8146e9181fe2f5c0
zhosibo <zhaosibo10654@navinfo.com>
2023-03-24 16:31:56 +0800

    1.test

--- app/src/main/java/com/lorizhao/adbtool/MainActivity.kt
+++ app/src/main/java/com/lorizhao/adbtool/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.lorizhao.adbtool
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Base64
 import com.lorizhao.adbtool.databinding.ActivityMainBinding
 import timber.log.Timber
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -28,38 +36,26 @@ class MainActivity : AppCompatActivity() {
             }else{
                 adb_status_off_int
             })
-
-            compoundButton.postDelayed({
-                val temp = Settings.Global.getInt(
-                    contentResolver,
-                    Settings.Global.ADB_ENABLED,
-                    adb_status_off_int
-                )
-                binding.switchADB.isChecked = temp == adb_status_on_int
-            }, 3000)
         }
 
 
         // 获取 USB 设备角色
-        val usbRole = getCommand(action_get_usb_status_string)
+        val usbRole = runCommand(action_get_usb_status_string)
         binding.switchUSB.isChecked = usbRole == usb_status_gadget
-
-        Timber.tag("usbRole").d("adbEnabled = $adbEnabled , usbRole = ${usbRole}")
         binding.switchUSB.setOnCheckedChangeListener { compoundButton, b ->
             if (b){
-                runCommand(action_put_usb_status_gadget)
+                executeShellCommand(action_put_usb_status_gadget)
             }else{
-                runCommand(action_put_usb_status_host)
+                executeShellCommand(action_put_usb_status_host)
             }
-            compoundButton.postDelayed({
-                val temp = getCommand(action_get_usb_status_string)
-                binding.switchUSB.isChecked = temp == usb_status_gadget
-            }, 3000)
+            testResult()
         }
+
+        testResult()
 
     }
 
-    private fun getCommand(command: String): String {
+    private fun runCommand(command: String): String {
         try {
             val process = Runtime.getRuntime().exec(command)
             val inputStream = process.inputStream
@@ -78,7 +74,69 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun runCommand(command: String){
-        Runtime.getRuntime().exec(command)
+    private fun executeShellCommand(command: String): String {
+        var output = ""
+        try {
+            val process = Runtime.getRuntime().exec("su")
+            val outputStream = process.outputStream
+            val outputStreamWriter = OutputStreamWriter(outputStream)
+            outputStreamWriter.write("$command\n")
+            outputStreamWriter.flush()
+            outputStreamWriter.write("exit\n")
+            outputStreamWriter.flush()
+
+            val inputStream = process.inputStream
+            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            val stringBuffer = StringBuffer()
+            var line: String?
+            while (bufferedReader.readLine().also { line = it } != null) {
+                stringBuffer.append(line)
+            }
+            output = stringBuffer.toString().trim()
+
+            process.waitFor()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+        return output
     }
+
+    fun testResult(){
+        val adbResult = Settings.Global.getInt(
+            contentResolver,
+            Settings.Global.ADB_ENABLED,
+            adb_status_off_int
+        )
+        val usbRole = runCommand(action_get_usb_status_string)
+        val isTestKey = isTestKeys()
+        Timber.tag("testResult").d("adbResult = $adbResult , usbRole = $usbRole,isTestKey = ${isTestKey}")
+    }
+
+    fun isTestKeys(): Boolean {
+        val pm = packageManager
+        val packageName = packageName
+        val flags = PackageManager.GET_SIGNATURES
+
+        try {
+            val packageInfo = pm.getPackageInfo(packageName, flags)
+            val signatures = packageInfo.signatures
+            for (signature in signatures) {
+                val raw = signature.toByteArray()
+                val md = MessageDigest.getInstance("SHA")
+                md.update(raw)
+                val signatureHash = Base64.encodeToString(md.digest(), Base64.DEFAULT)
+                if (signatureHash.contains("test-keys")) {
+                    return true
+                }
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        }
+        return false
+    }
+
 }
